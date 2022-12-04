f88ddeefae819b936ed53fcdc6d5482391f402e1
Nattan <nattan078@gmail.com>
2022-12-03 21:35:30 -0300

    Forma final do projeto

--- js/produtos.js
+++ js/produtos.js
@@ -26,10 +26,115 @@ function ativaSalgados() {
     document.getElementById( 'doces' ).style.display = 'none';
 }
 
-function ativaInformacoes() {
-    document.getElementById( 'container-informacoes' ).style.display = 'flex';
+function ativaCappucino() {
+    document.getElementById( 'cappuccino' ).style.display = 'flex';
 }
 
-function escondeInformacoes() {
-    document.getElementById( 'container-informacoes' ).style.display = 'none';
+function ativaDonut() {
+    document.getElementById( 'donuts' ).style.display = 'flex';
+}
+
+function ativaExpresso() {
+    document.getElementById( 'expresso' ).style.display = 'flex';
+}
+
+function ativaChocolateQuente() {
+    document.getElementById( 'chocolate-quente' ).style.display = 'flex';
+}
+
+function ativaAmericano() {
+    document.getElementById( 'americano' ).style.display = 'flex';
+}
+
+function ativaCafeGelado() {
+    document.getElementById( 'cafe-gelado' ).style.display = 'flex';
+}
+
+function ativaChaMatte() {
+    document.getElementById( 'cha-matte' ).style.display = 'flex';
+}
+
+function ativaMilkShake() {
+    document.getElementById( 'milkshakecard' ).style.display = 'flex';
+}
+
+function ativaAffogato() {
+    document.getElementById( 'affogatocard' ).style.display = 'flex';
+}
+
+function ativaBrownie() {
+    document.getElementById( 'brownie' ).style.display = 'flex';
+}
+
+function ativaCookie() {
+    document.getElementById( 'cookie' ).style.display = 'flex';
+}
+function ativaTortaLimao() {
+    document.getElementById( 'torta-limao' ).style.display = 'flex';
+}
+function ativaBrigadeiro() {
+    document.getElementById( 'brigadeiro' ).style.display = 'flex';
+}
+function ativaPaoDeQueijo() {
+    document.getElementById( 'pao-de-queijo' ).style.display = 'flex';
+}
+function ativaPastel() {
+    document.getElementById( 'pastel' ).style.display = 'flex';
+}
+function ativaCoxinha() {
+    document.getElementById( 'coxinha' ).style.display = 'flex';
+}
+function ativaSanduiche() {
+    document.getElementById( 'sanduiche' ).style.display = 'flex';
+}
+function escondeCappuccino() {
+    document.getElementById( 'cappuccino' ).style.display = 'none';
+}
+function escondeExpresso() {
+    document.getElementById( 'expresso' ).style.display = 'none';
+}
+function escondeChocolateQuente() {
+    document.getElementById( 'chocolate-quente' ).style.display = 'none';
+}
+function escondeAmericano() {
+    document.getElementById( 'americano' ).style.display = 'none';
+}
+function escondeCafeGelado() {
+    document.getElementById( 'cafe-gelado' ).style.display = 'none';
+}
+function escondeChaMatte() {
+    document.getElementById( 'cha-matte' ).style.display = 'none';
+}
+function escondeMilkshake() {
+    document.getElementById( 'milkshakecard' ).style.display = 'none';
+}
+function escondeAffogato() {
+    document.getElementById( 'affogatocard' ).style.display = 'none';
+}
+function escondeBrownie() {
+    document.getElementById( 'brownie' ).style.display = 'none';
+}
+function escondeCookie() {
+    document.getElementById( 'cookie' ).style.display = 'none';
+}
+function escondeTortaLimao() {
+    document.getElementById( 'torta-limao' ).style.display = 'none';
+}
+function escondeBrigadeiro() {
+    document.getElementById( 'brigadeiro' ).style.display = 'none';
+}
+function escondePaoDeQueijo() {
+    document.getElementById( 'pao-de-queijo' ).style.display = 'none';
+}
+function escondePastel() {
+    document.getElementById( 'pastel' ).style.display = 'none';
+}
+function escondeCoxinha() {
+    document.getElementById( 'coxinha' ).style.display = 'none';
+}
+function escondeSanduiche() {
+    document.getElementById( 'sanduiche' ).style.display = 'none';
+}
+function escondeDonut() {
+    document.getElementById( 'donuts' ).style.display = 'none';
 }
